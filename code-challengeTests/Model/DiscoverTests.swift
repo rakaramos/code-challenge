7d1e86d35c227e4a3bf34f38f2854da2c6f7390a
Rafael Machado <raka.cdc@gmail.com>
2018-10-13 08:25:33 -0300

    Fix tests

--- code-challengeTests/Model/DiscoverTests.swift
+++ code-challengeTests/Model/DiscoverTests.swift
@@ -34,39 +34,30 @@ class ModelMappingTests: XCTestCase {
                         }
                       ]
                     }
-                    """.data(using: .utf8)
-
-        guard let data = input else {
-            return XCTFail("Unable to convert from String to Data")
-        }
+                    """.data(using: .utf8)!
 
         let decoder = JSONDecoder()
 
-        do {
-            let discover = try decoder.decode(Product<Discover>.self, from: data)
+        let discover = try! decoder.decode(Product<Discover>.self, from: input)
 
-            XCTAssertTrue(discover.currentPage == 1)
-            XCTAssertTrue(discover.pages == 93)
-            XCTAssertTrue(discover.count == 1856)
+        XCTAssertEqual(discover.currentPage, 1)
+        XCTAssertEqual(discover.pages, 93)
+        XCTAssertEqual(discover.count, 1856)
 
-            let first = discover.results.first
+        let first = discover.results.first
 
-            XCTAssertTrue(first?.identifier == 156022)
-            XCTAssertTrue(first?.votes == 4136)
-            XCTAssertTrue(first?.isVideo == false)
-            XCTAssertTrue(first?.title == "The Equalizer")
-            XCTAssertTrue(first?.popularity == 24.573)
-            XCTAssertTrue(first?.poster == "/2eQfjqlvPAxd9aLDs8DvsKLnfed.jpg")
-            XCTAssertTrue(first?.originalLanguage == "en")
-            XCTAssertTrue(first?.originalTitle == "The Equalizer")
-            XCTAssertTrue(first?.genre == [53, 28, 80])
-            XCTAssertTrue(first?.backdrop == "/hEJ52KqwOmyRpcihs10h7xOwN7e.jpg")
-            XCTAssertTrue(first?.isAdult == false)
-            XCTAssertTrue(first?.releaseDate == "2014-09-24")
-            XCTAssertTrue(first?.overview == "In The Equalizer, Denzel Washington plays McCall, a man who believes he has put his mysterious past behind him and dedicated himself to beginning a new, quiet life. But when McCall meets Teri (Chloë Grace Moretz), a young girl under the control of ultra-violent Russian gangsters, he can’t stand idly by – he has to help her. Armed with hidden skills that allow him to serve vengeance against anyone who would brutalize the helpless, McCall comes out of his self-imposed retirement and finds his desire for justice reawakened. If someone has a problem, if the odds are stacked against them, if they have nowhere else to turn, McCall will help. He is The Equalizer.")
-
-        } catch {
-            XCTFail("\(error)\nCan't cast to \(String(describing: Product<Discover>.self))")
-        }
+        XCTAssertEqual(first?.identifier, 156022)
+        XCTAssertEqual(first?.votes, 4136)
+        XCTAssertEqual(first?.isVideo, false)
+        XCTAssertEqual(first?.title, "The Equalizer")
+        XCTAssertEqual(first?.popularity, 24.573)
+        XCTAssertEqual(first?.poster, "/2eQfjqlvPAxd9aLDs8DvsKLnfed.jpg")
+        XCTAssertEqual(first?.originalLanguage, "en")
+        XCTAssertEqual(first?.originalTitle, "The Equalizer")
+        XCTAssertEqual(first?.genres, [53, 28, 80])
+        XCTAssertEqual(first?.backdrop, "/hEJ52KqwOmyRpcihs10h7xOwN7e.jpg")
+        XCTAssertEqual(first?.isAdult, false)
+        XCTAssertEqual(first?.releaseDate, "2014-09-24")
+        XCTAssertEqual(first?.overview, "In The Equalizer, Denzel Washington plays McCall, a man who believes he has put his mysterious past behind him and dedicated himself to beginning a new, quiet life. But when McCall meets Teri (Chloë Grace Moretz), a young girl under the control of ultra-violent Russian gangsters, he can’t stand idly by – he has to help her. Armed with hidden skills that allow him to serve vengeance against anyone who would brutalize the helpless, McCall comes out of his self-imposed retirement and finds his desire for justice reawakened. If someone has a problem, if the odds are stacked against them, if they have nowhere else to turn, McCall will help. He is The Equalizer.")
     }
 }
